c740509a26599a815d649f23fe8390f4b6a40a93
Damian Karaś <damian.karas@3mdeb.com>
2022-02-04 11:14:40 +0100

    .gitlab-ci: add check hw matrix job before regression
    Signed-off-by: Damian Karaś <damian.karas@3mdeb.com>

---
+++ .gitlab-ci/check_hw_matrix.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone https://$GITLAB_ROBOT_USERNAME:$GITLAB_ROBOT_TOKEN@$TESTS_REPO_GIT
+cd $TESTS_REPO_DIR
+
+sed -i 's+git@gitlab.com:3mdeb/rte/rtectrl-rest-api.git+https://'$GITLAB_ROBOT_USERNAME':'$GITLAB_ROBOT_TOKEN'@gitlab.com/3mdeb/rte/rtectrl-rest-api.git+' .gitmodules
+sed -i 's+git@gitlab.com:3mdeb/rte/snipeit-rest-api.git+https://'$GITLAB_ROBOT_USERNAME':'$GITLAB_ROBOT_TOKEN'@gitlab.com/3mdeb/rte/snipeit-rest-api.git+' .gitmodules
+git submodule update --init --checkout
+
+# legacy or mainline?
+if [[ $FIRMWARE_VERSION =~ v4\.0.* ]]; then
+    export FIRMWARE="l"
+else
+    export FIRMWARE="m"
+fi
+
+bash -cx "./check_hw_matrix.sh ${RELEASE_DIR}/${PLATFORM}_${CI_COMMIT_REF_NAME}.rom"
