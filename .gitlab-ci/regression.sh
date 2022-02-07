4ac727a710adaae6125bd79d3f3ba05614638365
Damian Karaś <damian.karas@3mdeb.com>
2022-02-07 08:33:04 +0100

    .gitlab-ci: correct checkout
    Signed-off-by: Damian Karaś <damian.karas@3mdeb.com>

--- .gitlab-ci/regression.sh
+++ .gitlab-ci/regression.sh
@@ -3,8 +3,6 @@
 git clone https://$GITLAB_ROBOT_USERNAME:$GITLAB_ROBOT_TOKEN@$TESTS_REPO_GIT
 cd $TESTS_REPO_DIR
 
-git checkout hw-matrix-check
-
 sed -i 's+git@gitlab.com:3mdeb/rte/rtectrl-rest-api.git+https://'$GITLAB_ROBOT_USERNAME':'$GITLAB_ROBOT_TOKEN'@gitlab.com/3mdeb/rte/rtectrl-rest-api.git+' .gitmodules
 sed -i 's+git@gitlab.com:3mdeb/rte/snipeit-rest-api.git+https://'$GITLAB_ROBOT_USERNAME':'$GITLAB_ROBOT_TOKEN'@gitlab.com/3mdeb/rte/snipeit-rest-api.git+' .gitmodules
 git submodule update --init --checkout
